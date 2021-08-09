ea325ca26446c499e6341531dc47d6f740e39b77
Oby Manyando <33107568+obymanyando@users.noreply.github.com>
2021-08-09 13:10:06 +0300

    untrack backend

--- backend/api/vaccinations.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import vaccinationsCtrl from './vaccinations.controller.js'
-
-const router = express.Router()
-
-router.route('/').get(vaccinationsCtrl.apiGetVaccinations)
-
-export default router
